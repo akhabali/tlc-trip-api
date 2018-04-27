719221fca681c87f3f89f695b993a4b99613b5d9
Anas KHABALI <akhabali@talend.com>
2018-04-27 13:15:08 +0100

    add trip endpoint with data sample

--- src/main/java/io/github/khabali/tlctrip/front/TApplication.java
+++ src/main/java/io/github/khabali/tlctrip/front/TApplication.java
@@ -4,4 +4,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("api")
-public class TApplication extends Application {}
+public class TApplication extends Application {
+
+
+}
--- src/main/java/io/github/khabali/tlctrip/front/TripResource.java
+++ src/main/java/io/github/khabali/tlctrip/front/TripResource.java
@@ -2,30 +2,111 @@ package io.github.khabali.tlctrip.front;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
-import io.github.khabali.tlctrip.model.Trip;
-
-import java.util.Collections;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.spi.JsonProvider;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 
 @Path("trip")
 @ApplicationScoped
 @Produces(APPLICATION_JSON)
 public class TripResource {
 
+    private static final Integer MAX_RESULT = 10000;
+
+    private final static List<JsonObject> trips = new ArrayList<>();
+
+    private final JsonProvider jsonp = JsonProvider.provider();
+
+    private final SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+
+    private void loadData() throws URISyntaxException, IOException, ParseException {
+        final URI uri;
+
+        uri = TripResource.class.getClassLoader()
+                .getResource("samples/yellow_tripdata_2017.csv")
+                .toURI();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(uri)))) {
+            String[] headers = br.readLine()
+                    .split(",");
+            String line;
+            while ((line = br.readLine()) != null) {
+                JsonObjectBuilder ob = jsonp.createObjectBuilder();
+                String[] values = line.split(",");
+                for (int i = 0; i < values.length; i++) {
+                    if (i == 1 || i == 2) { //date
+                        final LocalDate localDate = df.parse(values[i]).toInstant()
+                                .atZone(ZoneId.systemDefault()).toLocalDate();
+                        ob.add(headers[i] + "_day", localDate.getDayOfMonth());
+                        ob.add(headers[i] + "_month", localDate.getMonthValue());
+                        ob.add(headers[i] + "_year", localDate.getYear());
+                        ob.add(headers[i], values[i]);
+                    } else if (i == 3 || i == 4) { // number
+                        ob.add(headers[i], Double.parseDouble(values[i]));
+                    } else {
+                        ob.add(headers[i], values[i]);
+                    }
+
+                }
+                trips.add(ob.build());
+            }
+        }
+    }
 
     @GET
-    public List<Trip> query(
-            @QueryParam("pickup_time") String pickupTime,
-            @QueryParam("passenger_count") String passengerCount,
-            @QueryParam("distance") Long distance,
-            @QueryParam("max_records") Integer maxRecords) {
+    public List<JsonObject> query(
+            @QueryParam("month") final Integer month,
+            @QueryParam("passenger_count") final Integer passengerCount,
+            @QueryParam("trip_distance") final Double distance,
+            @QueryParam("max_records") final Integer maxRecords) {
+        if (trips.isEmpty()) {
+            try {
+                loadData();
+            } catch (URISyntaxException | IOException | ParseException e) {
+                throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
+                        .entity("Can't load data samples\n" + e.getMessage())
+                        .build());
+            }
+        }
 
-        return Collections.emptyList();
+        if (month == null || month > 12 || month < 1) {
+            throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
+                    .entity("month value is not valid '" + month + "'")
+                    .build());
+        }
+
+        Integer maxResult = maxRecords;
+        if (maxResult == null || maxResult > MAX_RESULT) {
+            maxResult = MAX_RESULT;
+        }
+
+        return trips.stream()
+                .filter(t -> t.getInt("tpep_pickup_datetime_month") == month)
+                .filter(t -> passengerCount == null || t.getInt("passenger_count") == passengerCount)
+                .filter(t -> distance == null || t.getJsonNumber("trip_distance").doubleValue() <= distance)
+                .limit(maxResult)
+                .collect(Collectors.toList());
     }
 
 }
